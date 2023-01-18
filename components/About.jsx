6f2461576c166e368dc89123361f464b568d6c8c
Maxi <mfallauto@gmail.com>
2023-01-18 02:10:33 -0300

    cambioi about

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <section className="about" id="about">
     <div className="image">
-      <img src="image/servicios.webp" alt="" />
+      <img src="image/servicios.png" alt="" />
     </div>
     <div className="content">
       <a href={`https://wa.me/3516890981`} className="btn">
