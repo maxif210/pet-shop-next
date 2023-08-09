b12737b4a058b8d2d8b8a06dd00c005e818ab3e3
Maxi <mfallauto@gmail.com>
2023-08-09 03:13:20 -0300

    title-cambio

--- components/HeadMeta.jsx
+++ components/HeadMeta.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
 
 export const HeadMeta = () => {
   return (
     <Head>
-    <meta charSet="UTF-8" />
-    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="Negocio productos rubro veterinaria" />
-    <title>Veterinaria Tucán</title>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-    />
-  </Head>
-  )
-}
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Negocio productos rubro veterinaria" />
+      <title>Veterinaria Tu-Can</title>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+      />
+    </Head>
+  );
+};
