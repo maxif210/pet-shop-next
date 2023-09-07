bba661183c5ae1771296fcde26027b615d381cf4
Maxi <mfallauto@gmail.com>
2023-09-06 21:23:26 -0300

    cambiosMobile

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,9 +4,19 @@ export const Hero = () => {
   return (
     <section className="home" id="home">
       <div className="content">
-        <a href="#shop" className="btn-home">
-          COMPRAR AHORA
-        </a>
+        <img
+          src="/image/banner-bienvenida-pc.png"
+          srcSet="/image/banner-bienvenida-pc.png 1400w,/image/banner-bienvenida-movil.png 768w"
+          sizes="(min-width: 1200px) 1400px,
+                (min-width: 768px) 768px,
+                100vw"
+          alt="ImagenHome"
+        ></img>
+        <div className="button-container">
+          <a href="#shop" className="btn-home">
+            COMPRAR AHORA
+          </a>
+        </div>
       </div>
     </section>
   );
